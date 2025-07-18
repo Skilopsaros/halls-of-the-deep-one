1968d22d80a3fa56b73d0cfcd961dbb1ab731f06
FlyingCheeseDemon <jakobbuehler@gmx.net>
2025-07-18 20:10:59 +0200

    removed the chest from the start of the game
    as we're moving towards actual gameplay

--- scenes/main.gd
+++ scenes/main.gd
@@ -11,9 +11,6 @@ func _ready() -> void:
 	var player_inventory:Inventory = inventory_manager.player_inventory
   
 	# example content to try functionality
-	var chest:Inventory = inventory_manager.add_inventory(5,3,"Chest")
-	chest.add_item(item_library.get_item_by_key("coin"),0)
-	chest.add_item(item_library.get_item_by_key("frog"),1)
 	player_inventory.add_item(item_library.get_item_by_key("coin"),9)
 	player_inventory.add_item(item_library.get_item_by_key("gem"),8)
 	player_inventory.add_item(item_library.get_item_by_key("shoe"),13)
