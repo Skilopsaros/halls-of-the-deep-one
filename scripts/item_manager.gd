c1e6fb6ea3c1f9f3c05b93f5a850d71653e35bab
FlyingCheeseDemon <jakobbuehler@gmx.net>
2026-02-05 21:36:19 +0100

    automatic generation of higher value items on runtime
    only the copper variant needs to be actually created

--- scripts/item_manager.gd
+++ scripts/item_manager.gd
@@ -2,6 +2,9 @@ extends Node
 
 var item_dictionary:Dictionary = {}
 
+const gold_material = preload("res://graphics/materials/gold.tres")
+const silver_material = preload("res://graphics/materials/silver.tres")
+
 func initialize_item_dict() -> void:
 	var dir := DirAccess.open("res://resources/items")
 	if dir == null:
@@ -9,15 +12,23 @@ func initialize_item_dict() -> void:
 		return
 	dir.list_dir_begin()
 	for file:String in dir.get_files():
-		print(dir.get_current_dir() + "/" + file)
 		var resource := load(dir.get_current_dir() + "/" + file)
 		if resource == null:
+			print("Failed to load " + file)
 			continue
 		item_dictionary[resource.name] = resource
 
 func get_item_by_name(key:String) -> ItemObject:
 	if len(item_dictionary.keys()) == 0:
-		initialize_item_dict()
+		initialize_item_dict();
+	var material:int = 0
+	if key.begins_with("silver_"):
+		material = 1
+		key = key.replace("silver","copper")
+	elif key.begins_with("gold_"):
+		material = 2
+		key = key.replace("gold","copper")
+		
 	var item:Item = item_dictionary[key]
 	var item_object := ItemObject.constructor(item)
 	if len(item.alternative_textures) != 0:
@@ -27,6 +38,13 @@ func get_item_by_name(key:String) -> ItemObject:
 			item_object.texture = item.alternative_textures[texture_selection-1]
 		else:
 			item_object.texture = item.texture
+	
+	if material == 1:
+		item_object.material = silver_material
+		item_object.data.value *= 5
+	elif material == 2:
+		item_object.material = gold_material
+		item_object.data.value *= 25
 	
 	if item is ContainerItem:
 		var inventory:Inventory = get_node("/root/Main").inventory_manager.add_inventory(item.cols,item.rows,item.title,false,true,true)
